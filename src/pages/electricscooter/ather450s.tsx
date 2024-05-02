fca516529665b28d0d7995cfe13f1aca4c8975d8
deeptanshugit <deeptanshupanthi26@gmail.com>
2024-05-02 19:04:11 +0530

    Added Vehicle Navigation Tab

--- src/pages/electricscooter/ather450s.tsx
+++ src/pages/electricscooter/ather450s.tsx
@@ -7,55 +7,65 @@ import RelatedVehicle from "@/app/components/carousel/RelatedVehicle/RelatedVehi
 import VehiclePrice from "@/app/components/carousel/VehiclePrice/vehicleprice";
 import VehicleName from "@/app/components/vehiclename/VehicleName";
 import CostCalculator from "@/app/components/costcalculator/CostCalculator";
+import styles from "./ather450s.module.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import VehicleNavigationTab from "@/app/components/tabs/VehicleNavigationTab/VehicleNavigationTab";
 
 const Ather450S = () => {
   const [vehicleData, setVehicleData] = useState([]);
-  const [prices, setPrices] = useState({} as any)
+  const [prices, setPrices] = useState({} as any);
 
   useEffect(() => {
     // Fetch vehicle data from your backend API
-    fetch(
-      "http://localhost:3001/api/v1/vehicles/6616255497bf58c85bf40d8b"
-    )
+    fetch("http://localhost:3001/api/v1/vehicles/6616255497bf58c85bf40d8b")
       .then((response) => {
         return response.json();
       })
       .then((data) => {
         setVehicleData(data.vehicle);
-        setPrices(data.prices)
+        setPrices(data.prices);
       })
       .catch((error) => console.error("Error fetching vehicle data:", error));
   }, []);
 
   return (
-    <div>
-      <div className="p-5 mt-0">
+    <div className={styles.bodyContainer}>
+      <div className="p-1 mt-2">
         <VehicleName name="Ather 450s"></VehicleName>
       </div>
 
-      <div className="p-5 mt-0">
-        <Featured imageURL="/electricscooter/ather/ather450s.png" prices={prices} />
+      <div className="p-1 mt-2" id="vehicleOverview">
+        <div className="mb-2">
+          <VehicleNavigationTab />
+        </div>
+        <Featured
+          imageURL="/electricscooter/ather/ather450s.png"
+          prices={prices}
+        />
       </div>
 
-      <div className="p-5">
+      <div className="p-1 mt-5" id="vehicleSpecificationTable">
         {vehicleData && <SpecificationTable data={vehicleData as any} />}
       </div>
 
-      <div className="p-5 mt-2">
+      <div className="p-1 mt-5" id="relatedVehicle">
         <RelatedVehicle></RelatedVehicle>
       </div>
 
-      <div className="p-5 mt-2">
+      <div className="p-1 mt-5" id="vehiclePrice">
         <VehiclePrice></VehiclePrice>
       </div>
 
-      <div className="p-5 mt-2">
-        <CostCalculator battery_capacity_kwh={2.9} range_km={100} vehicleName={"Ather 450S"}></CostCalculator>
+      <div className="p-1 mt-5" id="vehicleCostCalculator">
+        <CostCalculator
+          battery_capacity_kwh={2.9}
+          range_km={100}
+          vehicleName={"Ather 450S"}
+        ></CostCalculator>
       </div>
 
-      <div className="p-5 mt-2">
+      <div className="p-1 mt-5" id="vehicleFAQ">
         <FAQS></FAQS>
       </div>
     </div>
